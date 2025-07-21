b4dcaffad9affdc396b25d8468c1e5444b85ea2b
amanpandey104 <36291873+amanpandey104@users.noreply.github.com>
2025-07-21 23:34:08 +0530

    initial commit

---
+++ src/main/java/placeorder/app/placeorder/PlaceOrderApplication.java
@@ -0,0 +1,13 @@
+package placeorder.app.placeorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaceOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaceOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/placeorder/app/placeorder/PlaceOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package placeorder.app.placeorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaceOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
