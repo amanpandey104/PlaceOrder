28afde8d7256aa7a26296439c7f57d0357dc894e
amanpandey104 <36291873+amanpandey104@users.noreply.github.com>
2025-07-22 14:36:57 +0530

    added column annotation on the entity

--- src/main/java/placeorder/app/placeorder/entity/OrderEntity.java
+++ src/main/java/placeorder/app/placeorder/entity/OrderEntity.java
@@ -2,6 +2,7 @@ package placeorder.app.placeorder.entity;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,16 +19,21 @@ public class OrderEntity {
 
     @Id 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
+     @Column(name = "customer_name", nullable = false, length = 255)
     private String customerName;
 
+    @Column(name = "items", nullable = false, length = 1000)
     private String items; // Comma-separated
 
+    @Column(name = "total_amount", nullable = false)
     private Double totalAmount;
 
+    @Column(name = "order_time", nullable = false)
     private LocalDateTime orderTime;
 
+    @Column(name = "status", nullable = false, length = 100)
     private String status;
-
 }
